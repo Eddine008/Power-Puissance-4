112fcf7dc989994fb921ac3e847668360a49618f
Eddinee <edssee008@gmail.com>
2025-10-27 19:20:38 +0100

    Initialisation des joueurs

--- src/main.go
+++ src/main.go
@@ -1,10 +1,25 @@
 package main
- 
+
 import (
-
+	Power4 "Power4/game"
+	"fmt"
 )
- func 
 
-func main () {
+// func initGrille(grille *[][]string) {
+// 	rows, cols := 6, 7
+// 	*grille = make([][]string, rows)
+// 	for i := 0; i < rows; i++ {
+// 		(*grille)[i] = make([]string, cols)
+// 		for j := 0; j < cols; j++ {
+// 			(*grille)[i][j] = "-"
+// 		}
+// 	}
+// }
 
+func main() {
+	var maGrille [][]string
+	Power4.InitGrille(&maGrille)
+	for _, ligne := range maGrille {
+		fmt.Println(ligne)
+	}
 }
