217b2db2fec2aafbafe50d023362d7c59b3b24fc
Eddinee <edssee008@gmail.com>
2025-11-24 11:23:10 +0100

    Main.go bon

--- src/main.go
+++ src/main.go
@@ -1,39 +1,56 @@
 package main
- 
+
 import (
+	Power4 "Power4/game"
+	"fmt"
 	"html/template"
 	"net/http"
-	"fmt"
 	"os"
-	Power4 "Power4/game"
 )
- 
-var templates *template.Template
- 
-func init() {
-	var err error
-	templates, err = template.ParseGlob("./templates/*.html")
-	if err != nil {
-		fmt.Println("Erreur templates :", err)
-		os.Exit(1)
-	}
-}
+
+var listTemplates *template.Template
 
 func main() {
-	http.HandleFunc("/",func(w func(http.ResponseWriter, *http.Request))) {
-		var grille [][]string
-		Power4.InitGrille(&grille)
 
-		templates.ExecuteTemplate(w, "home",grille)
+	var errTemplate error
+	listTemplates, errTemplate = template.ParseGlob("./templates/*.html")
+	if errTemplate != nil {
+		fmt.Println(errTemplate.Error())
+		os.Exit(1)
 	}
 
-		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
- 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var grille [][]string
+		Power4.InitGrille(&grille)
+		err := listTemplates.ExecuteTemplate(w, "base", grille)
+		if err != nil {
+			http.Error(w, "Erreur template base", http.StatusInternalServerError)
+			fmt.Println("Erreur template base :", err)
+			return
+		}
+	})
+
+	http.HandleFunc("/game/play", func(w http.ResponseWriter, r *http.Request) {
+		var grille [][]string
+		Power4.InitGrille(&grille)
+		err := listTemplates.ExecuteTemplate(w, "game", grille)
+		if err != nil {
+			http.Error(w, "Erreur template game", http.StatusInternalServerError)
+			fmt.Println("Erreur template game :", err)
+			return
+		}
+	})
+
+	http.HandleFunc("/game/scoreboard", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Page scoreboard (à compléter).")
+	})
+
+	http.HandleFunc("/game/init", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Page d'initialisation (à compléter avec un formulaire).")
+	})
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
 	fmt.Println("Serveur lancé sur http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("localhost:8080", nil)
 }
-
-
- 
-
-
